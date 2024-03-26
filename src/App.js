5e9248cd7a7a02a8cedc8d2c64fd6d926fab1dcd
Pelumi <imoshood47@gmail.com>
2024-03-26 11:57:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const api = "https://api.adviceslip.com/advice";
+
+  const [advice, setAdvice] = useState({});
+  const [isLoading, setIsLoading] = useState(false); // State to track loading status
+
+  function fetchData() {
+    setIsLoading(true); // Set loading to true when fetching starts
+    fetch(api)
+      .then((response) => response.json())
+      .then((data) => {
+        setAdvice(data.slip);
+        setIsLoading(false); // Set loading to false when fetching completes
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setIsLoading(false); // Set loading to false in case of error
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(advice);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="App flex justify-center items-center h-screen ">
+      <div className="bg-darkGrayishBlue p-8 flex flex-col justify-center items-center rounded-xl w-96 max-w-screen-md">
+        <div className="text-neonGreen text-center tracking-wide	leading-tight mb-3">
+          Advice #<span>{advice.id}</span>
+        </div>
+        <h1 className="font-extrabold text-2xl text-lightCyan text-center break-all text-pretty">
+          "{advice.advice}"
+        </h1>
+
+        <span className="divider"></span>
+        <button
+          className="bg-neonGreen rounded-full h-10 w-10 -mb-12 p-2 text-center  active:scale-90"
+          onClick={() => fetchData()}
+          disabled={isLoading} // Disable button when data is being fetched
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            className={"text-center " + (isLoading ? "animate-spin" : "")} // Conditionally apply animate-spin class
+            src="/images/icon-dice.svg"
+            alt=""
+          />
+        </button>
+      </div>
     </div>
   );
 }
